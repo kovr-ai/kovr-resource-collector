4482d6534bb5e0d3848b55d00fa0a472ecc4b963
surajboniwal <surajboniwal18@gmail.com>
2025-05-29 11:10:23 +0530

    script change

--- data_collector_script.sh
+++ data_collector_script.sh
@@ -27,7 +27,7 @@ fi
 curl -O https://raw.githubusercontent.com/kovr-ai/kovr-resource-collector/refs/heads/main/data_collector_requirements.txt
 
 # Install requirements
-pip install -r data_collector_requirements.txt
+pip install -r data_collector_requirements.txt --user
 
 # Download script
 curl -O https://raw.githubusercontent.com/kovr-ai/kovr-resource-collector/refs/heads/main/data_collector.py
