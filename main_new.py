dbed30713921787d490419979330dfa3d09ec2fe
Vishesh 'ironeagle' Bangotra <vishesh@kovr.ai>
2025-08-07 12:08:14 +0530

    fixes

--- main_new.py
+++ main_new.py
@@ -41,7 +41,7 @@ def main(
         executed_check_results,
         resource_collection=resource_collection
     )
-    return
+    # return
     # Print comprehensive summary
     sql.insert_check_results(
         check_dicts,
