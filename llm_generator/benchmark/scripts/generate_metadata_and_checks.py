3cc596e745893571625e0f75347c20144905f60d
Vishesh 'ironeagle' Bangotra <vishesh@kovr.ai>
2025-08-23 13:27:42 +0530

    benchmark skeleton

---
+++ llm_generator/benchmark/scripts/generate_metadata_and_checks.py
@@ -0,0 +1,7 @@
+"""
+excepts benchmark name as args
+calls the exposed services and generates metadata and checks metadata
+stores yaml files in data folder
+data/benchmarks/<benchmark_name>/metadata.yaml
+data/benchmarks/<benchmark_name>/checks/<check_unique_id>.yaml
+"""
