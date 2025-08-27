bbfae2919715dc783ca438a8a995517776233761
Vishesh 'ironeagle' Bangotra <vishesh@kovr.ai>
2025-08-27 08:59:26 +0530

    feat: implement llm_generator_v2 yaml loader and service framework
    - Add yaml_loader.py with dynamic Pydantic model generation from YAML schemas
    - Add service framework base class with execution lifecycle
    - Add benchmark_and_checks_literature module structure
    - Support for automatic input/output model creation from execution.yaml
    - Fix type mapping and reference resolution for nested YAML structures

--- llm_generator_v2/benchmark_and_checks_literature/generate_benchmark_literature/services.py
+++ llm_generator_v2/benchmark_and_checks_literature/generate_benchmark_literature/services.py
@@ -1,8 +1,12 @@
-from pydantic import BaseModel
 from llm_generator_v2.services import Service
+
+from .models import (
+    Input,
+    Output
+)
 
 
 class GenerateBenchmarkLiterature(Service):
-    def _process_input(self, input_) -> BaseModel:
+    def _process_input(self, input_: Input) -> Output:
         raise NotImplementedError()
 
---
+++ llm_generator_v2/services.py
@@ -0,0 +1,60 @@
+from pydantic import BaseModel
+from llm_generator_v2.yaml_loader import ServiceYamlField
+
+
+class Service:
+    def __init__(
+            self,
+            service_config: ServiceYamlField,
+    ):
+        self._service_config = service_config
+        self.index = self._service_config.index
+        self.name = self._service_config.name
+        self.input_model = self._service_config.input_model
+        self.output_model = self._service_config.output_model
+
+    def _process_input(self, input_) -> BaseModel:
+        """
+        service code to be extended by Service classes
+        """
+        raise NotImplementedError()
+
+    def _prepare_input(self, input_: BaseModel):
+        """
+        makes sure the input_ adheres to service input model
+        should be in base class
+        """
+        raise NotImplementedError()
+
+    def _save_input(self, input_: BaseModel):
+        """
+        this should be part of base class to read from data files
+        and prepare full input for process function
+        should be in base class
+        """
+        raise NotImplementedError()
+
+    def _prepare_output(self, output: BaseModel):
+        """
+        makes sure the output adheres to service output model
+        should be in base class
+        """
+        raise NotImplementedError()
+
+    def _save_output(self, output: BaseModel):
+        """
+        saves output file in set data directory
+        should be in base class
+        """
+        raise NotImplementedError()
+
+    def execute(self, input_: BaseModel):
+        input_ = self._prepare_input(input_)
+        self._save_input(input_)
+
+        output_dict = self._process_input(input_)
+
+        output = self._prepare_output(output_dict)
+        self._save_output(output)
+
+        return output
